f930dcf8cf89fe0585e88252f14a3bc31a1dff98
LukeZHar <15649@coderacademy.edu.au>
2024-11-19 23:15:48 +1000

    Feat: Setting up a simple React app

---
+++ README.md
@@ -0,0 +1,6 @@
+# Term 3 Week 5 - Intro to React
+
+- Tuesday:
+    - CRA (Create React App)
+
+    
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+const foods = ['hamburger', 'pizza', 'ramen', 'sushi', 'hotdog', 'ice cream'];
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Our Menu!</h1>
+      <ul>
+        {
+          foods.map((food) => {
+            return <li>{food}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+
+export default App;
